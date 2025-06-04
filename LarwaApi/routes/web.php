ee4015b53767e21d7b002d4afaeaf5b87c7ed3e0
ZofD <lukasz.kwasny@interia.pl>
2025-06-04 21:10:12 +0200

    dodanie routingu

--- LarwaApi/routes/web.php
+++ LarwaApi/routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\OrderDetailController;
+
+Route::apiResource('orders', OrderController::class);
+Route::apiResource('products', ProductController::class);
+Route::apiResource('order-details', OrderDetailController::class);
 
 Route::get('/', function () {
     return view('welcome');
